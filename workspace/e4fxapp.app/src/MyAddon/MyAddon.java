bfa50da6a18ba4408292dec545225b6eaef2b6b2
tfdllc253 <tfdllc253@TFDL-pc10132.uc.ucalgary.ca>
2017-04-01 16:36:12 -0600

    getPersistedState

--- workspace/e4fxapp.app/src/MyAddon/MyAddon.java
+++ workspace/e4fxapp.app/src/MyAddon/MyAddon.java
@@ -1,13 +1,18 @@
 package MyAddon;
 
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.model.application.MAddon;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
 public class MyAddon {
 	@Inject
-	MyAddon(IEventBroker eventBroker, EModelService modelService){
+	MyAddon(IEventBroker eventBroker, EModelService modelService, MAddon addon){
+		Map<String, String> persist = addon.getPersistedState();
+		System.out.println(persist);
 		System.out.println("In myAddon");
 	}
 }
