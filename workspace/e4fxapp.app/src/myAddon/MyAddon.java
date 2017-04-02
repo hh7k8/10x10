685cc4478051504bf46714ce157d39e33a9ba3fe
tfdllc253 <tfdllc253@TFDL-pc10132.uc.ucalgary.ca>
2017-04-02 14:22:21 -0600

    sleep, running 2 addons with the same Class URI
    They start up 1 after the other

--- workspace/e4fxapp.app/src/myAddon/MyAddon.java
+++ workspace/e4fxapp.app/src/myAddon/MyAddon.java
@@ -1,4 +1,4 @@
-package MyAddon;
+package myAddon;
 
 import java.util.Map;
 
@@ -8,11 +8,30 @@ import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.MAddon;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
+
+
 public class MyAddon {
+	
+	private final EModelService modelService;
+	
 	@Inject
 	MyAddon(IEventBroker eventBroker, EModelService modelService, MAddon addon){
 		Map<String, String> persist = addon.getPersistedState();
 		System.out.println(persist);
 		System.out.println("In myAddon");
-	}
+		
+		this.modelService = modelService;
+//		eventBroker.subscribe("TEST", this::handleWidget);
+
+		/*		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+*/
+		}
+//	void handleWidget(Event event){
+//		
+//	}
 }
