8aa6a704f98c6727252e0180d558c0bcfb9915bf
tfdllc253 <tfdllc253@TFDL-pc10129.uc.ucalgary.ca>
2017-04-08 17:18:44 -0600

    Extended org.eclipse.core.runtime.applications
    Note that org.eclipse.equinox.app had to be added to Imported Packages
    so NonUI would compile.  Also needed to add -application="comm" to
    Program arguments to run configuration

--- workspace/e4fxapp.app/src/nonUI/NonUI.java
+++ workspace/e4fxapp.app/src/nonUI/NonUI.java
@@ -1,14 +1,16 @@
 package nonUI;
 
-import javax.annotation.PostConstruct;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
 
-import javafx.scene.layout.BorderPane;
+public class NonUI implements IApplication {
 
-public class NonUI {
-	@PostConstruct
-	void init(BorderPane pane){
-
- 		System.err.println("in NonUI");
+	 public Object start(IApplicationContext app) {
+ 		System.err.println("starting in NonUI");
+		return app;
 	
 	}
+	 public void stop() {
+		 System.err.println("stopping in NonUI");
+	 }
 }
